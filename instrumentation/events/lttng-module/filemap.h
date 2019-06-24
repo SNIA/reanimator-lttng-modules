2757eb54bcf5d302350f94f1ae613d3124c81b90
Umit <umit@fsl.cs.sunysb.edu>
2019-06-23 22:49:32 -0400

    new tracepoints added to fsl lttng

--- instrumentation/events/lttng-module/filemap.h
+++ instrumentation/events/lttng-module/filemap.h
@@ -14,31 +14,67 @@
 #include <linux/kdev_t.h>
 #include <linux/errseq.h>
 
-LTTNG_TRACEPOINT_EVENT_CLASS(mm_filemap_op_page_cache,
+LTTNG_TRACEPOINT_EVENT_CLASS(
+	mm_filemap_op_page_cache,
 
 	TP_PROTO(struct page *page),
 
 	TP_ARGS(page),
 
-        TP_FIELDS(
+	TP_FIELDS(
 		ctf_integer(unsigned long, pfn, page_to_pfn(page))
 		ctf_integer(unsigned long, i_ino, page->mapping->host->i_ino)
-	        ctf_integer(unsigned long, index, page->index)
-	        ctf_integer(dev_t, s_dev, page->mapping->host->i_sb ?
-                            page->mapping->host->i_sb->s_dev :
-                            page->mapping->host->i_rdev)
-	)
+		ctf_integer(unsigned long, index, page->index)
+		ctf_integer(dev_t, s_dev, page->mapping->host->i_sb
+					    ? page->mapping->host->i_sb->s_dev
+					    : page->mapping->host->i_rdev)
+                  )
 )
 
-LTTNG_TRACEPOINT_EVENT_INSTANCE(mm_filemap_op_page_cache, mm_filemap_delete_from_page_cache,
-	TP_PROTO(struct page *page),
-	TP_ARGS(page)
-	)
+LTTNG_TRACEPOINT_EVENT_INSTANCE(mm_filemap_op_page_cache,
+				mm_filemap_delete_from_page_cache,
+				TP_PROTO(struct page *page),
+				TP_ARGS(page)
+)
 
-LTTNG_TRACEPOINT_EVENT_INSTANCE(mm_filemap_op_page_cache, mm_filemap_add_to_page_cache,
-	TP_PROTO(struct page *page),
-	TP_ARGS(page)
-	)
+LTTNG_TRACEPOINT_EVENT_INSTANCE(mm_filemap_op_page_cache,
+				mm_filemap_add_to_page_cache,
+				TP_PROTO(struct page *page),
+				TP_ARGS(page)
+)
+
+
+LTTNG_TRACEPOINT_EVENT(
+	filemap_set_wb_err,
+	TP_PROTO(struct address_space *mapping, errseq_t eseq),
+
+	TP_ARGS(mapping, eseq),
+
+	TP_FIELDS(
+		ctf_integer(unsigned long, i_no, mapping->host->i_ino)
+		ctf_integer(errseq_t, errseq, eseq)
+		ctf_integer(dev_t, s_dev, mapping->host->i_sb
+					      ? mapping->host->i_sb->s_dev
+					      : mapping->host->i_rdev)
+                  )
+)
+
+LTTNG_TRACEPOINT_EVENT(
+	file_check_and_advance_wb_err,
+	TP_PROTO(struct file *file, errseq_t old),
+
+	TP_ARGS(file, old),
+
+	TP_FIELDS(
+		ctf_integer(errseq_t, old, old)
+		ctf_integer(errseq_t, new, file->f_wb_err)
+		ctf_integer(unsigned long, i_no, file->f_mapping->host->i_ino)
+		ctf_integer(struct file *, file, file)
+		ctf_integer(dev_t, s_dev, file->f_mapping->host->i_sb
+					  ? file->f_mapping->host->i_sb->s_dev
+					  : file->f_mapping->host->i_rdev)
+                  )
+)
 
 #endif /* LTTNG_TRACE_FILEMAP_H */
 
